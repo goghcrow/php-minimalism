d8638fa30e966d53458eef84083a8f98a09bb335
chuxiaofeng <chuxiaofeng@youzan.com>
2017-02-05 23:34:48 +0800

    pretty print

--- test/Interpret/SExp.php
+++ test/Interpret/SExp.php
@@ -31,7 +31,6 @@ $sexp = <<<'SEXP'
       (string-append "Hello " who))))
 
 (echo (say-hello null))
-(echo "\n")
 (echo (say-hello "chuxiaofeng"))
 SEXP;
 
@@ -39,8 +38,33 @@ file_put_contents("tmp", $sexp);
 register_shutdown_function(function() { @unlink("tmp"); });
 
 
+// todo 处理引用递归
+function pretty_print($exp, $dep = 0)
+{
+    if (is_array($exp)) {
+        $isNumArr = array_keys($exp) === range(0, count($exp) - 1);
+        $arr = [];
+        foreach ($exp as $key => $value) {
+            if ($isNumArr) {
+                $arr[] = pretty_print($value, $dep + 1);
+            } else {
+                $arr[] = "'$key' => " . pretty_print($value, $dep + 1);
+            }
+        }
+        if ($dep === 0) {
+            return "[" . implode(", ", $arr) . "]";
+        } else {
+            $offset = str_repeat(" ", $dep);
+            return "\n{$offset}[" . implode(", ", $arr) . "]";
+        }
+    } else {
+        return var_export($exp, true);
+    }
+}
+
+
 $interp = new YInterp();
 $ast = $interp->__toAst("tmp");
-//echo json_encode($ast, JSON_PRETTY_PRINT), "\n";
+echo pretty_print($ast), "\n\n";
 $r = interp($ast);
-//// var_dump($r);
+// var_dump($r);
